2ab7bb149ba764585f6e9fc04e397221e1c79f82
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-05 17:54:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ const getUserBtn = document.getElementById("getUserBtn");
 const userDiv = document.getElementById("user");
 
 getUserBtn.addEventListener("click", () => {
-  fetch('https://randomeuser.me/api/')
-  .then(response => response.json())
-  .then(data => {
-    const user = date.results[0];
-    userDiv.innerHTML = `
-      <img src="${user.picture.large}" alt="User Picture">
-      <h2>${user.name.title} ${user.name.first} ${user.name.last}</h2>
-      <p>Email: ${user.email}</p>
-      <p>Location: ${user.location.city} ${user.location.countery} </p>
-  `;
-    console.log(user);
-  }
-        .catch(error => {
-          userDiv.innerHTML = <p style="color:red;">Error fetching user</p>;
-          console.error(error);
-        });
+  fetch('https://randomuser.me/api/')
+    .then(response => response.json())
+    .then(data => {
+      const user = data.results[0];
+      userDiv.innerHTML = `
+        <img src="${user.picture.large}" alt="User Picture">
+        <h2>${user.name.title} ${user.name.first} ${user.name.last}</h2>
+        <p>Email: ${user.email}</p>
+        <p>Location: ${user.location.city}, ${user.location.country}</p>
+      `;
+      console.log(user); // See the full data in browser console
+    })
+    .catch(error => {
+      userDiv.innerHTML = `<p style="color:red;">Error fetching user</p>`;
+      console.error(error);
+    });
 });
