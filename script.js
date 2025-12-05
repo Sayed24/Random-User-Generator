d47902d52d3da61364a4f54a8bf9a41b5995a828
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-05 17:49:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const getUserBtn = document.getElementById("getUserBtn");
+const userDiv = document.getElementById("user");
 
+getUserBtn.addEventListener("click", () => {
+  fetch('https://randomeuser.me/api/')
+  .then(response => response.json())
+  .then(data => {
+    const user = date.results[0];
+    userDiv.innerHTML = `
+      <img src="${user.picture.large}" alt="User Picture">
+      <h2>${user.name.title} ${user.name.first} ${user.name.last}</h2>
+      <p>Email: ${user.email}</p>
+      <p>Location: ${user.location.city} ${user.location.countery} </p>
+  `;
+    console.log(user);
+  }
+        .catch(error => {
+          userDiv.innerHTML = <p style="color:red;">Error fetching user</p>;
+          console.error(error);
+        });
+});
