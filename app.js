898b69daed113368338c6c8c1beb818629c166a2
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-05 18:12:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ getUserBtn.addEventListener("click", () => {
         <img src="${user.picture.large}" alt="User Picture">
         <h2>${user.name.title} ${user.name.first} ${user.name.last}</h2>
         <p>Email: ${user.email}</p>
+        <p>Phone: ${user.phone}</p>
         <p>Location: ${user.location.city}, ${user.location.country}</p>
       `;
       console.log(user); // See the full data in browser console
